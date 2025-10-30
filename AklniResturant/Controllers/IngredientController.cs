873ac84a711f203e9a732733ad7219ff4acd13ff
assembakr <asem.abdelhamid@ejust.edu.eg>
2025-10-30 22:31:31 +0300

    added an edit view in ingredient controller

--- AklniResturant/Controllers/IngredientController.cs
+++ AklniResturant/Controllers/IngredientController.cs
@@ -2,6 +2,7 @@
 using AklniResturant.Models;
 using AklniResturant.Repos;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 
 namespace AklniResturant.Controllers
 {
@@ -74,12 +75,39 @@ namespace AklniResturant.Controllers
         public async Task<IActionResult> Delete(Ingredient i)
         {
             await _ingredients.DeleteAsync(i.IngredientId);
-
+            await _ingredients.SaveChangesAsync();
             if (i == null)
             {
                 return NotFound();
             }
             return RedirectToAction("Index");
+        }
+
+        // edit view to edit specific ingredient
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var query = new Query<Ingredient>() { Includes = "ProdIngredients.Product"};
+            var ingred = await _ingredients.GetByIdAsync(id, query);
+
+            if (ingred == null)
+            {
+                return NotFound();
+            }
+            return View(ingred);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Ingredient i)
+        {
+            if (ModelState.IsValid)
+            {
+                await _ingredients.UpdateAsync(i);
+                await _ingredients.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(i);
         }
     }
 }
